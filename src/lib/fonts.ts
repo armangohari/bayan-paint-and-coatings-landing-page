6bab10be7b32f2a2576b6d0e272187c56dca4806
armangohari <arman.gohari.2001@gmail.com>
2024-12-06 11:47:15 +0330

    feat: add yekan bakh font

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,5 +1,90 @@
 import local from "next/font/local";
 
+export const yekanBakhFont = local({
+  src: [
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/thin.woff",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/light.woff",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/semibold.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/semibold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/extrabold.woff",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/extrabold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/black.woff",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff/extrablack.woff",
+      weight: "950",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/yekan-bakh/woff2/extrablack.woff2",
+      weight: "950",
+      style: "normal",
+    },
+  ],
+});
+
 export const iranSansFont = local({
   src: [
     {
